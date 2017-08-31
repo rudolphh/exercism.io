a0b3a8eca4fc1165485e24fdf52921a414a1d15e
Rudolph Hernandez <rudolpharthur@gmail.com>
2017-08-30 18:47:33 -0700

    new matrix solution

--- javascript/matrix/matrix.js
+++ javascript/matrix/matrix.js
@@ -1,36 +1,10 @@
 
 function Matrix(matrix){
-  this.rows = [];
+  this.rows = matrix.split('\n').map( row => row.split(' ').map(num => +num) );
   this.columns = [];
-
-  let endline = matrix.indexOf('\n');
-  let remainder = matrix;
-  let row;
-
-  while( remainder !== ''){
-
-    if(endline !== -1) {
-      row = remainder.slice(0, endline);
-      remainder = remainder.slice(endline+1);
-      endline = remainder.slice(0).indexOf('\n');
-    }
-    else {
-      row = remainder.slice(0);
-      remainder = '';
-    }
-
-    this.rows.push(row.split(' ').map((number) => +number));
-  }
-
-  let col = [];
-  for(let i = 0; i < this.rows.length; i++){
-    for(let j = 0; j < this.rows.length; j++){
-      col.push(this.rows[j][i]);
-    }
-    this.columns.push(col);
-    col = [];
-  }
-
+  this.rows.forEach((elem, i) => {
+		this.columns[i] = this.rows.map(item => item[i]);
+  });
 }
 
 module.exports = Matrix;
